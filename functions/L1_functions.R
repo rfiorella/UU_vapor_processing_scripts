8faf7f38a1fe3c2726d8b96fbccb8b1b597915b0
Rich Fiorella <rich.fiorella@utah.edu>
2017-02-21 23:37:53 -0700

    Migrate L1 functions and script to new repo.

---
+++ functions/L1_functions.R
@@ -0,0 +1,171 @@
+# Picarro Water Vapor Isotope Data Reduction 
+# L1 Functions
+
+# migrated from WBB-vapor-scripts repo by rich fiorella on 21/2/17.
+# code in these functions written by rich fiorella based on work by galen gorski
+# (likely some of galen's functions have made it through with no changes!)
+
+# contact info: rich fiorella, rich.fiorella@utah.edu
+
+# this script contains a series of accessory functions for the L1 script.
+# as of 21/2/17, this script has been designed to work only with a Picarro L2130.
+# other analyzer versions will likely require modification of this script.
+
+# load any required packages for these functions
+library(parallel) # allow for parallel computing
+library(compiler) # allow functions to be precompiled.
+library(lubridate) # include useful time functions
+##################################################################################
+# determine the number of cores available for parallel computing
+
+no_cores <- detectCores() - 1 # but leave on thread available so as not to crash computer
+							  # this is important if runnning on a local computer, but -1 
+							  # can be removed if running on a dedicated cluster
+print(paste(Sys.time(),"Number of available cores for parallel processing:",no_cores))
+#----------------------------------------------------------------------------------
+# L1 functions follow....
+#------------------------------------------------------------------------------------
+# extract.date.from.L0.files - break down file names to provide just the date
+# back as the return value. input is a list of file names - can have a full directory
+# path, output is a list of just dates. from this, the indices matching each month can 
+# easily be extracted.
+
+extract.date.from.L0.files <- function(file.list) {
+  # suppress warnings in this function - as.numeric will create NAs by coercion,
+  # and this will be reported as a warning to the screen every time. But since this
+  # is precisely what we are hoping to do, warnings are unncessary here.
+  oldw <- getOption("warn")
+  options(warn = -1)
+  # split file name into pieces using *underscores*
+  file.name.string.pieces <- strsplit(file.list,split="_")
+  # extract dates from resulting string pieces
+  file.name.dates <- ymd(unlist(file.name.string.pieces))
+  # remove NAs
+  file.name.dates.woNAs <- file.name.dates[!is.na(file.name.dates)]
+  # set warnings to old status
+  options(warn = oldw)
+  # return date list
+  return(file.name.dates.woNAs)
+}
+
+#------------------------------------------------------------------------------------
+# concatenate.to.monthly - load all files that are within a single month, and
+# concatenate to a single monthly data frame. requires as an input a list of files to 
+# concatenate, and returns the monthly data frame.
+
+concatenate.to.monthly.uncompiled <- function(month.list,useParallel=TRUE) {
+  print(paste(Sys.time()," Concatenating daily L0 files to a single monthly file..."))
+  # require that indices be of type list...
+  stopifnot(is.list(month.list))
+
+  # make a vector of files
+  files <- unlist(month.list)
+
+  if (useParallel==TRUE) { # run function in parallel
+  	cl <- makeCluster(no_cores)
+  	d <- parLapply(cl,files,function(x){read.table(x,header=TRUE,stringsAsFactors=FALSE,sep=",")})
+  	stopCluster(cl)
+
+	output <- do.call(rbind,d)
+
+	# clean up memory 
+	rm(d)
+	gc()
+  } else { # run function in serial...
+ 	# concatenate all files together
+  	output <- read.table(files[1],header=TRUE,stringsAsFactors=FALSE,sep=",")
+  	# if month has more than one day of data, 
+  	if (length(files)>1){
+    	for (i in (2:length(files))) {
+    	temp <- read.table(files[i],header=TRUE,stringsAsFactors=FALSE,sep=",")
+    	output <- rbind(output,temp)
+    	} # end for
+    } # end length(files) if statement
+  } # end (isParallel) if statement
+
+  # return concatenated data
+  return(output)
+}
+
+concatenate.to.monthly <- cmpfun(concatenate.to.monthly.uncompiled)
+
+#-----------------------------------------------------------------------------------
+# qflag function - read in data table, run a quick check. this varies from 
+# older versions of qflag in the tolerance - reduced digits parameter from 
+# 2 to 0 - changes tolerance from ±0.005°C or ±0.005 torr to ±0.5°C and ±0.5 torr
+# in quick tests, this reduced the amount of data rejected from ~70% to <1%.
+# old tolerances seem to be unnecessarily strict.
+
+# input is the monthly data frame, output is the monthly data frame with flagged
+# points removed.
+
+qflag.uncompiled <- function(x){
+  print(paste(now()," Removing points that fail instrument quality checks..."))
+  # save number of rows initially to calculate how many data points are removed.
+  begin <- nrow(x)
+  # use the round function to identify points that will round to 80°C or 50 torr
+  x = x[(round(x$CavityTemp, digits = 0) == 80),]
+  x = x[(round(x$CavityPressure, digits = 0) == 50),]
+  # find how many rows remain that pass the rounding test
+  end <- nrow(x)
+  # calculate the percentage of points that bave been removed, and print to the screen
+  print(paste(100*(1-end/begin),"% of data points removed"))
+  # return the filtered data frame and the percentage of points removed
+  output <- list("data"=x,"pct.discarded"=round(100*(1-end/begin),2))
+  # return list
+  return(output)
+}
+
+qflag <- cmpfun(qflag.uncompiled)
+#-----------------------------------------------------------------------
+# data.sanity.check function
+# common sense data flag - pull out values where the output makes no sense
+# (e.g., impossible delta or H2o values). This function is explicitly conservative
+# to only remove values that are extremely improbably to be correct...
+
+data.sanity.check.uncompiled <- function(x) {
+  print(paste(now()," Removing points that fail data sanity checks..."))
+  # save number of initial rows for future calculations
+  begin <- nrow(x)
+  # apply data sanity standards - at a minimum, values should be:
+  # (a) for H2O, values should be positive.
+  # (b) for d18O, values MUST be above -1000, and ALMOST CERTAINLY will be below 50
+  # (c) for d2H, values MUST be above -1000, and ALMOST CERTAINLY will be below 400
+  x = x[(x$H2O>=0),] 
+  x = x[(x$Delta_18_16 > -1000),]
+  x = x[(x$Delta_D_H > -1000),]
+  x = x[(x$Delta_18_16 < 50),]
+  x = x[(x$Delta_D_H < 400),]
+  # how many points pass these tests?
+  end <- nrow(x)
+  # calculate the percentage of points that have been removed, and print to stdout
+  print(paste(100*(1-end/begin),"% of data points removed"))
+  # return the filtered data frame and the percentage of points removed
+  output <- list("data"=x,"pct.discarded"=round(100*(1-end/begin),2))
+  # return output list
+  return(output)
+}
+
+data.sanity.check <- cmpfun(data.sanity.check.uncompiled)
+
+#----------------------------------------------------
+# create attach.L0.Header function
+# this function creates the header for datafiles and attaches the metadata
+# loaded into a csv file.
+
+attach.L0.Header.uncompiled <- function(output_filename,metadata_dataframe) {
+  sink(output_filename)
+  cat(paste("# ",nrow(metadata_dataframe)+4,"\n",sep=""))
+  cat("# BEGIN HEADER \n")
+  for (i in 1:nrow(metadata_dataframe)) {
+    	cat(paste("# ",metadata_dataframe[i,1],", ",metadata_dataframe[i,2],"\n",sep=""))   	
+  }
+  cat(paste("# date.scripts.run, ",base::date(),"\n"))
+  # date.scripts.run flag modified on 2feb17 from date() to base:date() to avoid conflicts
+  # if the lubridate package is active.
+  cat("# END HEADER \n")
+  sink()
+}
+
+# compile the function
+attach.L0.Header <- cmpfun(attach.L0.Header.uncompiled)
---
+++ scripts/L1_Script.R
@@ -0,0 +1,227 @@
+# L1_Script.R
+# L1 script, richf 
+# heavily borrowing script and algorithms from L1 scripts by Galen Gorski.
+
+# starting from a fresh code here since: (a) forces me to go through and understand all old code,
+# (b) I think there are some major coding-philosophy differences to be sorted out
+# (c) structure of L0 files changed in ways that might not be directly compatible.
+
+# there are a few *GIGANTIC* functions in the old L1 functions file. This code will likely be more
+# readable if broken into smaller functions.
+
+# clear workspace
+rm(list=ls())
+
+# load libraries
+library(lubridate)
+
+# load associated functions
+source("../functions/L1_functions.R")
+
+# Set user variables
+start.date <- ymd("2015-01-01")
+end.date <- ymd("2017-02-01")
+
+path.to.L0.data <- "~/WBB_VAPOR/L0/testing/"
+path.to.output.L1.data <-  "~/WBB_VAPOR/L1/testing/"
+######################################################################################################################################
+# SET METADATA
+# write out metadata to help data curation - these will be appended to the top of the data files currently. it might be possible in
+# later versions to use this to write out an xml file?
+######################################################################################################################################
+
+metadata.frame <- read.csv("../metadata_templates/L0_WBB_metadata.csv",header=TRUE)
+
+##########################################################################
+# LOAD DAILY DATA, CONCATENATE TO MONTHLY, THEN SEPARATE INTO 
+# CALIBRATION AND AMBIENT MEASUREMENT PERIODS
+##########################################################################
+period <- interval(start.date,end.date)
+
+nmonths <- period %/% months(1)
+
+print("==================================================================")
+print(paste("Starting L1 Processing : ",now()))
+print("==================================================================")
+
+# Make lists of relevant *CALIBRATION* data files
+#---------------------------------------------------
+# make an empty list to hold all raw file names
+cal.file.list <- list()
+
+# list of all raw files
+cal.file.list <- list.files(path=path.to.L0.data,pattern="CalibData",full.names=TRUE,recursive=TRUE)
+
+# find month for each file
+cal.file.dates <- extract.date.from.L0.files(cal.file.list)	
+
+# subset list based on period of interest
+cal.subset <- cal.file.list[cal.file.dates %within% period]
+
+# Make lists of relevant *CALIBRATION* data files
+#---------------------------------------------------
+# make an empty list to hold all raw file names
+amb.file.list <- list()
+
+# list of all raw files
+amb.file.list <- list.files(path=path.to.L0.data,pattern="AmbientData",full.names=TRUE,recursive=TRUE)
+
+# find month for each file
+amb.file.dates <- extract.date.from.L0.files(amb.file.list) 
+
+# subset list based on period of interest
+amb.subset <- amb.file.list[amb.file.dates %within% period]
+
+# initiate log - need to keep track of how much data has been removed.
+log.yyyy <- vector()
+log.mm 	 <- vector()
+log.pct  <- vector()
+
+# initiate vector to hold data
+file.list.by.month <- vector("list",nmonths)
+#-----------------------------------------------------------------------------
+# loop through months for calibration data.
+#-----------------------------------------------------------------------------
+
+for (i in 1:nmonths) {
+	# print status header.
+	print("==================================================================")
+	print(paste("Processing L1 calibration data for month:",month(start.date %m+% months(i-1)),"/",year(start.date %m+% months(i-1))))
+	print("==================================================================")
+
+	# CONCATENATE CALIBRATION DATA FIRST.
+  	file.list.by.month[[i]] <- as.list(cal.file.list[month(cal.file.dates)==month(start.date %m+% months(i-1)) &
+  		year(cal.file.dates)==year(start.date %m+% months(i-1))])
+
+	# check to see if data exists for month in question
+	if (length(file.list.by.month[[i]])==0) {
+		print(paste("No data for :",month(start.date %m+% months(i-1)),"/",year(start.date %m+% months(i-1))))
+		next
+	}
+
+	# concatenate to monthly array
+	mondata <- concatenate.to.monthly(file.list.by.month[[i]])
+
+	# rough instrument status check and data sanity checks.
+	mondata.qflag <- qflag(mondata)
+	# clean up memory and gc
+	rm(mondata)
+	gc()
+
+	# NOTE: $data required here since output of qflag is a list!
+	mondata.filtered <- data.sanity.check(mondata.qflag$data) 
+
+	# get data row for log file - 
+	log.yyyy[i] <- year(start.date %m+% months(i-1))
+	log.mm[i] <- month(start.date %m+% months(i-1))
+	# small bug here to fix at some point - the reference points for these are different,
+	# so ultimately, this value can exceed 100% in rare cases when all of the data fails
+	# one check or the other.
+	log.pct[i] <- mondata.filtered$pct.discarded + mondata.qflag$pct.discarded
+
+	# clean up memory and gc
+	rm(mondata.qflag)
+	gc()
+
+ 	# Write calibration data file.
+ 	##########################################################################
+
+ 	if (nrow(mondata.filtered$data) > 0) {
+ 		print(paste(now()," Writing out calibration data frame..."))
+ 		# generate output filename
+ 		coutput.fname <- paste(path.to.output.L1.data,"WBB_Water_Vapor_CalibData_L1_",
+ 	  	  (start.date %m+% months(i-1)),"_",metadata.frame$Value[metadata.frame$Variable=="code.version"],".dat",sep="")
+
+ 		# attach metadata
+ 		attach.L0.Header(coutput.fname,metadata.frame)
+
+ 		# write out data portion of the data file.
+ 		write.table(mondata.filtered$data,file=coutput.fname,sep=",",append = TRUE,row.names=FALSE)
+ 	} else {
+ 		print(paste(now(), "No data passed filters for this month...."))
+ 	}
+
+ 	# clean up calibration data and gc
+ 	rm(mondata.filtered)
+ 	gc()
+}
+
+# clean up remaining variables corresponding to calibration data. 
+rm(file.list.by.month)
+gc()
+
+# re-initiate vector to hold data
+file.list.by.month <- vector("list",nmonths)
+
+#-----------------------------------------------------------------------------
+# loop through months for ambient data.
+#-----------------------------------------------------------------------------
+
+for (i in 1:nmonths) {
+
+	# print status header.
+	print("==================================================================")
+	print(paste("Processing L1 ambient data for month:",month(start.date %m+% months(i-1)),"/",year(start.date %m+% months(i-1))))
+	print("==================================================================")
+
+	# CONCATENATE AMIENT DATA SECOND.
+  	file.list.by.month[[i]] <- as.list(amb.file.list[month(amb.file.dates)==month(start.date %m+% months(i-1)) &
+  		year(amb.file.dates)==year(start.date %m+% months(i-1))])
+
+	# check to see if data exists for month in question
+	if (length(file.list.by.month[[i]])==0) {
+		print(paste("No data for :",month(start.date %m+% months(i-1)),"/",year(start.date %m+% months(i-1))))
+		next
+	}
+
+	# concatenate to monthly array
+	mondata <- concatenate.to.monthly(file.list.by.month[[i]])
+
+	# rough instrument status check and data sanity checks.
+	mondata.qflag <- qflag(mondata)
+	# clean up memory and gc
+	rm(mondata)
+	gc()
+
+	# NOTE: $data required here since output of qflag is a list!
+	mondata.filtered <- data.sanity.check(mondata.qflag$data) 
+
+	# get data row for log file - 
+	log.yyyy[i] <- year(start.date %m+% months(i-1))
+	log.mm[i] <- month(start.date %m+% months(i-1))
+	# small bug here to fix at some point - the reference points for these are different,
+	# so ultimately, this value can exceed 100% in rare cases when all of the data fails
+	# one check or the other.
+	log.pct[i] <- mondata.filtered$pct.discarded + mondata.qflag$pct.discarded
+
+	# clean up memory and gc
+	rm(mondata.qflag)
+	gc()
+
+ 	# Write ambient data file.
+ 	##########################################################################
+
+ 	if (nrow(mondata.filtered$data) > 0) {
+ 		print(paste(now()," Writing out ambient data frame..."))
+ 		# generate output filename
+ 		coutput.fname <- paste(path.to.output.L1.data,"WBB_Water_Vapor_AmbientData_L1_",
+ 	  	  (start.date %m+% months(i-1)),"_",metadata.frame$Value[metadata.frame$Variable=="code.version"],".dat",sep="")
+
+ 		# attach metadata
+ 		attach.L0.Header(coutput.fname,metadata.frame)
+
+ 		# write out data portion of the data file.
+ 		write.table(mondata.filtered$data,file=coutput.fname,sep=",",append = TRUE,row.names=FALSE)
+ 	} else {
+ 		print(paste(now(), "No data passed filters for this month...."))
+ 	}
+
+ 	# clean up calibration data and gc
+ 	rm(mondata.filtered)
+ 	gc()
+}
+
+Rprof(NULL)
+# log.fname <- paste("L1_log",".dat",sep="")
+# log.output <- data.frame(log.yyyy,log.mm,log.pct)
+# write.table(log.output,file=log.fname,sep=",")
