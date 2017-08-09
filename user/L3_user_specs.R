10e7f711a70197340f6d2e154a44fa6140112a3e
Rich Fiorella <rich.fiorella@utah.edu>
2017-08-08 20:29:27 -0600

    Add option to force calibration to ends of data
    Adds option to force calibration to ends of ambient data. Essentially carries
    first calibration backward and last calibration to end if the data stream
    isn't perfectly bookended by calibration data. Closes #28.

--- user/L3_user_specs.R
+++ user/L3_user_specs.R
@@ -3,15 +3,15 @@
 
 # this file contains all of the parameters that need to be specified for the L3 data processing script.
 
-start.date <- ymd("2013-12-01")
-end.date <- ymd("2017-03-01")
+start.date <- ymd("2015-12-01")
+end.date <- ymd("2016-03-01")
 
 # where is the uncalibrated ambient vapor data?
 path.to.L1.data <- "~/VaporData/WBB_VAPOR/L1/v110beta/"
 
 # id calibration data to use:
-path.to.L2.calib.data <- "~/VaporData/WBB_VAPOR/L2/v110beta/"
-calib.file <- "WBB_Water_Vapor_CalibrationRegressionData_L2_2013-05-01_2017-05-01.dat"
+path.to.L2.calib.data <- "~/VaporData/WBB_VAPOR/L2/v110beta/w1314/"
+calib.file <- "WBB_Water_Vapor_CalibrationData_L2_2015-11-01_2016-03-01.rds"
 
 # where should output data go?
 path.to.output.L3.data <-  "~/VaporData/WBB_VAPOR/L3/v110beta/"
@@ -22,3 +22,24 @@ debug <- 0 # 0 for no debugging, >0 for (increasingly verbose) debugging
 
 # output time frequency (uses xts)
 out.interval <- 5 # minutes.
+
+#-------------------------------------------
+# How to handle gaps in calibration regressions?
+#-------------------------------------------
+
+# a. should we force the first regression period in calibration
+# file to correspond to the beginning of the ambient period 
+# being processed?
+force.to.beginning <- TRUE 
+
+# b. should we force the last regression period in calibration
+# file to correspond to the end of the ambient period being
+# processed?
+force.to.end <- TRUE
+
+# c. how should we deal with gaps in the calibration file?
+# three possible options: (a) omit, (b) carry last calibration
+# forward, (c) carry next calibration backward.
+gap.handling <- "last.carried.forward" 	# omit, 
+										# last.carried.forward, 
+										# or next.carried.backward
